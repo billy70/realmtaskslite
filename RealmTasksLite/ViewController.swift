919ab5787d81868c8b92178f0de1f8accbf6c939
William L. Marr III <hobbyist@wlmiii.net>
2016-12-28 14:06:13 -0500

    Add test data to the table view using a Realm List

--- RealmTasksLite/ViewController.swift
+++ RealmTasksLite/ViewController.swift
@@ -27,13 +27,32 @@ final class TaskList: Object {
 }
 
 class ViewController: UITableViewController {
+    var items = List<Task>()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+
+        // FIXME: test data to be removed at some later point...
+        items.append(Task(value: ["text": "My First Task"]))
     }
 
     func setupUI() {
         title = "My Tasks"
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+
+    // MARK: UITableView
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let item = items[indexPath.row]
+        cell.textLabel?.text = item.text
+        cell.textLabel?.alpha = item.completed ? 0.5 : 1
+        return cell
     }
 }
