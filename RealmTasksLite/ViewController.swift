45b63139676f1426633420fc3876854eb48c974e
William L. Marr III <hobbyist@wlmiii.net>
2016-12-28 12:23:57 -0500

    Initial Commit

---
+++ RealmTasksLite/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmTasksLite
+//
+//  Created by William L. Marr III on 2016/12/28.
+//  Copyright © 2016 William L. Marr III. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
